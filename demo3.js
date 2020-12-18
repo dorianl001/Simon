d49b31ff4f797d868d07147bb53edc492357556a
dorianl <dorian.lopez001@gmail.com>
2020-12-18 11:43:42 -0500

    files added to simon

---
+++ demo3.js
@@ -0,0 +1,42 @@
+//buttons
+// let red = document.getElementById("red-button");
+// let green = document.getElementById("green-button");
+// let blue = document.getElementById("blue-button");
+
+// //click event 
+// red.addEventListener("click", function(event) {
+//     event.preventDefault();
+// }
+
+// let recordedSeq = [];
+//variable simon is the sequence
+//add randomization
+let initialSeq = Array(4).fill().map((element, index) => index + 0);
+console.log(initialSeq)
+
+
+//add intervals
+for (let i = 0; i < 4; i++) {
+    setTimeout( () => console.log(initialSeq[i]), i * 3000)
+}
+
+//dom manipulation for buttons to light up in a random sequence 
+
+
+
+//get loop to flash the button and or div in a sequence  
+
+
+
+
+
+// user pushes button one 
+// function clickButtonOne() {
+    
+// }
+
+// function alertMe() {
+//     alert("times up you lose!!")
+// }
+
+// const stopTimerId = setInterval(alertMe, 3000);
